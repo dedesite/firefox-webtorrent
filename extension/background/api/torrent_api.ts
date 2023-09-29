fbf58f11e434f993360f7f9ed11c83b3fb0e551a
Andréas Livet <andreas.livet@gmail.com>
2023-09-29 21:09:25 +0200

    Fix parseTorrentRemote

--- extension/background/api/torrent_api.ts
+++ extension/background/api/torrent_api.ts
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-import ParseTorrent from 'parse-torrent'
+import * as ParseTorrent from 'parse-torrent'
 
 export const parseTorrentRemote = (torrentId: string, tabId: number) => {
   ParseTorrent.remote(torrentId, (err: Error, parsedTorrent?: ParseTorrent.Instance) => {
