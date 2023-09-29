4c2c8eba3be95f550e296ff41bb3818795d0a02d
Andréas Livet <andreas.livet@gmail.com>
2023-09-29 12:16:25 +0200

    Also pack background
    Still doesn't work

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const webpack = require("webpack");
 module.exports = {
   entry: {
     main: "./extension/firefox_webtorrent.tsx",
+    background: "./extension/background/store.ts",
   },
   module: {
     rules: [
@@ -30,15 +31,25 @@ module.exports = {
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
     fullySpecified: false,
+    fallback: {
+      crypto: require.resolve("crypto-browserify"),
+      stream: require.resolve("stream-browserify"),
+      timers: require.resolve("timers-browserify"),
+      dns: require.resolve("chrome-dns"),
+      dgram: require.resolve("chrome-dgram"),
+    },
   },
   output: {
     path: path.resolve(__dirname, "extension/out/"),
-    filename: "bundle.js",
+    filename: "[name].bundle.js",
   },
   plugins: [
-    new webpack.DefinePlugin({
-      "process.env": "{}",
+    new webpack.ProvidePlugin({
+      process: "process/browser",
     }),
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+    })
   ],
   mode: "none",
   devtool: "source-map",
